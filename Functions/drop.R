0fe6f46a2c45130035dcc97636f587c1b5b042ca
Andrew Shaughnessy <shaughnessyar@slu.edu>
2017-05-25 13:24:45 -0500

    Drop function
    function to drop any number of observations from the begining or end of a dataset to account for equilibration and time out of the water. There is an option to only remove the begining or end as well as a control on the number of observations dropped from either end

---
+++ Functions/drop.R
@@ -0,0 +1,16 @@
+drop <- function(dataframe,a=NULL,b=NULL){
+  Output <- dataframe
+  if (is.null(a)){
+    Output <- Output [-c((nrow(Output)-`b`):nrow(Output)),]
+    return(Output)
+  }
+  else if (is.null(b)) {
+    Output <- Output [-c(1:`a`), ]
+    return(Output)
+  }
+  else
+  Output <- Output [-c((nrow(Output)-`b`):nrow(Output)),]
+  Output <- Output [-c(1:`a`), ]
+  return(Output)
+
+}
