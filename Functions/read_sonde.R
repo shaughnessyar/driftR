eed9e71c3861edf01b6c0edba622f0aeb37dbabd
Andrew Shaughnessy <shaughnessyar@slu.edu>
2017-07-02 10:51:10 -0400

    added an option to not define the variables if you do not want to.

--- Functions/read_sonde.R
+++ Functions/read_sonde.R
@@ -1,9 +1,26 @@
-read.sonde <- function(extention) {
-  library(readr)
-  all_content = readLines(extention)
-  skip_second = all_content[-2]
-  df = read.csv(textConnection(skip_second), header = TRUE, stringsAsFactors = FALSE)
-  df$Date <- as.Date(df$Date, "%m/%d/%y")
-  df$Time <- parse_time(df$Time,format='%H:%M:%S')
-  return(df)
+read.sonde <- function(extention, define=NULL) {
+  if (is.null(define)) {
+    library(readr)
+    all_content = readLines(extention)
+    skip_second = all_content[-2]
+    df = read.csv(textConnection(skip_second), header = TRUE, stringsAsFactors = FALSE)
+    df$Date <- as.Date(df$Date, "%m/%d/%y")
+    df$Time <- parse_time(df$Time,format='%H:%M:%S')
+    return(df)
+  }
+  else if (define == "FALSE"){
+    library(readr)
+    all_content = readLines(extention)
+    df = read.csv(textConnection(all_content), header = TRUE, stringsAsFactors = FALSE)
+    return(df)
+  }
+  else {
+    library(readr)
+    all_content = readLines(extention)
+    skip_second = all_content[-2]
+    df = read.csv(textConnection(skip_second), header = TRUE, stringsAsFactors = FALSE)
+    df$Date <- as.Date(df$Date, "%m/%d/%y")
+    df$Time <- parse_time(df$Time,format='%H:%M:%S')
+    return(df)
+  }
 }
