00a7ca809808dcc6cc2147e57353ec50d2f84a7e
Andrew Shaughnessy <shaughnessyar@slu.edu>
2017-07-02 10:57:17 -0400

    creates an option to not define variables as date or time

--- Functions/read_exo.R
+++ Functions/read_exo.R
@@ -1,7 +1,20 @@
-read.exo <- function(extention, x) {
-  library(readr)
-  df <- read_csv(extention, skip = x)
-  df$'Date (MM/DD/YYYY)' <- as.Date(df$'Date (MM/DD/YYYY)', "%m/%d/%y")
-  df$'Time (HH:MM:SS)' <- parse_time(df$'Time (HH:MM:SS)',format='%H:%M:%S')
-  return(df)
+read.exo <- function(extention, x , define=NULL) {
+  if (is.null(define)) {
+    library(readr)
+    df <- read_csv(extention, skip = x)
+    df$'Date (MM/DD/YYYY)' <- as.Date(df$'Date (MM/DD/YYYY)', "%m/%d/%y")
+    df$'Time (HH:MM:SS)' <- parse_time(df$'Time (HH:MM:SS)',format='%H:%M:%S')
+    return(df)
+  }
+  else if (define == "FALSE"){
+    library(readr)
+    df <- read_csv(extention, skip = x)
+    return(df)
+  }
+  else {
+    library(readr)
+    df <- read_csv(extention, skip = x)
+    df$'Date (MM/DD/YYYY)' <- as.Date(df$'Date (MM/DD/YYYY)', "%m/%d/%y")
+    df$'Time (HH:MM:SS)' <- parse_time(df$'Time (HH:MM:SS)',format='%H:%M:%S')
+    return(df)
 }
