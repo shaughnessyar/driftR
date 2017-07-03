dfa1612d5aed278703a93eb8c607747d9211d04a
Andrew Shaughnessy <shaughnessyar@slu.edu>
2017-07-03 10:27:35 -0400

    Edits to import functions

--- Functions/read_exo.R
+++ Functions/read_exo.R
@@ -1,20 +1,16 @@
-read.exo <- function(extention, x , define=NULL) {
+read.exo <- function(extention, x , define=FALSE) {
   if (is.null(define)) {
-    library(readr)
+    df <- read_csv(extention, skip = x)
+    return(df)
+  }
+  else if (define == "TRUE"){
     df <- read_csv(extention, skip = x)
     df$'Date (MM/DD/YYYY)' <- as.Date(df$'Date (MM/DD/YYYY)', "%m/%d/%y")
     df$'Time (HH:MM:SS)' <- parse_time(df$'Time (HH:MM:SS)',format='%H:%M:%S')
-    return(df)
-  }
-  else if (define == "FALSE"){
-    library(readr)
-    df <- read_csv(extention, skip = x)
     return(df)
   }
   else {
-    library(readr)
     df <- read_csv(extention, skip = x)
-    df$'Date (MM/DD/YYYY)' <- as.Date(df$'Date (MM/DD/YYYY)', "%m/%d/%y")
-    df$'Time (HH:MM:SS)' <- parse_time(df$'Time (HH:MM:SS)',format='%H:%M:%S')
     return(df)
+  }
 }
--- Functions/read_sonde.R
+++ Functions/read_sonde.R
@@ -1,26 +1,20 @@
-read.sonde <- function(extention, define=NULL) {
+read.sonde <- function(extention, define=FALSE) {
   if (is.null(define)) {
-    library(readr)
-    all_content = readLines(extention)
-    skip_second = all_content[-2]
-    df = read.csv(textConnection(skip_second), header = TRUE, stringsAsFactors = FALSE)
-    df$Date <- as.Date(df$Date, "%m/%d/%y")
-    df$Time <- parse_time(df$Time,format='%H:%M:%S')
-    return(df)
-  }
-  else if (define == "FALSE"){
-    library(readr)
     all_content = readLines(extention)
     df = read.csv(textConnection(all_content), header = TRUE, stringsAsFactors = FALSE)
     return(df)
   }
-  else {
-    library(readr)
+  else if (define == "TRUE"){
     all_content = readLines(extention)
     skip_second = all_content[-2]
     df = read.csv(textConnection(skip_second), header = TRUE, stringsAsFactors = FALSE)
     df$Date <- as.Date(df$Date, "%m/%d/%y")
     df$Time <- parse_time(df$Time,format='%H:%M:%S')
+    return(df)
+  }
+  else {
+    all_content = readLines(extention)
+    df = read.csv(textConnection(all_content), header = TRUE, stringsAsFactors = FALSE)
     return(df)
   }
 }
