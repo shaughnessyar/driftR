45d8518dd7519215c518463e256817d6d32570b1
Andrew Shaughnessy <shaughnessyar@slu.edu>
2017-07-06 19:24:05 -0400

    tibble edits

--- Functions/read_sonde.R
+++ Functions/read_sonde.R
@@ -2,6 +2,7 @@ read.sonde <- function(extention, define=FALSE) {
   if (is.null(define)) {
     all_content = readLines(extention)
     df = read.csv(textConnection(all_content), header = TRUE, stringsAsFactors = FALSE)
+    df <- as_tibble(df)
     return(df)
   }
   else if (define == "TRUE"){
@@ -10,11 +11,13 @@ read.sonde <- function(extention, define=FALSE) {
     df = read.csv(textConnection(skip_second), header = TRUE, stringsAsFactors = FALSE)
     df$Date <- as.Date(df$Date, "%m/%d/%y")
     df$Time <- parse_time(df$Time,format='%H:%M:%S')
+    df <- as_tibble(df)
     return(df)
   }
   else {
     all_content = readLines(extention)
     df = read.csv(textConnection(all_content), header = TRUE, stringsAsFactors = FALSE)
+    df <- as_tibble(df)
     return(df)
   }
 }
