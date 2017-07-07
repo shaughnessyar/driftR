0ecf65434157d8e81c9ad767d442f2079b8e26ce
Christopher Prener <prenercg@slu.edu>
2017-07-06 21:43:10 -0500

    proposed solution to day-time format

--- Functions/corr.frac.R
+++ Functions/corr.frac.R
@@ -1,9 +1,23 @@
-corr.frac <- function(dataframe, DateVar, TimeVar, Structure){
+corr.frac <- function(dataframe, DateVar, TimeVar, format){
   date <- eval(substitute(DateVar), dataframe)
   time <- eval(substitute(TimeVar), dataframe)
   datetime <- paste(date, time)
-  datetime <- as.POSIXct(datetime, format = Structure)
+  
+  # set format
+  if (format == "MDY"){
+    dayTimeFormat <- "%m/%d/%Y %H:%M:%S"
+  }
+  else if (format == "YMD"){
+    dayTimeFormat <- "%Y-%m-%d %H:%M:%S"
+  }
+  else {
+    stop("invalid date-time format - use either MDY or YMD")
+  }
+    
+  # apply format
+  datetime <- as.POSIXct(datetime, format = dayTimeFormat)
   datetime <- as.numeric(datetime)
+  
   tot_time <- tail(datetime,n=1) - head(datetime, n=1)
   corr.frac <- (datetime-head(datetime, n=1))/tot_time
   return(corr.frac)
