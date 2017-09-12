2c86ba910e3867edbca6da6e4ae98e1c0db9f039
Andrew Shaughnessy <shaughnessyar@slu.edu>
2017-09-12 10:21:00 -0500

    Delete read_usgs.R

--- Functions/read_usgs.R
+++
@@ -1,8 +0,0 @@
-read.usgs <- function(extention,x,Qvar) {
-  library(readr)
-  df <- read_csv(extention, skip = x)
-  all_content = readLines(df)
-  skip_second = all_content[-2]
-  df2 = read.csv(textConnection(skip_second), header = TRUE, stringsAsFactors = FALSE)
-  return(df2)
-}
