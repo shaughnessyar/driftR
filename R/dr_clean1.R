86e9ca6a9a49193e7fcfb3a1f886c78b3aaeb587
Andrew Shaughnessy <shaughnessyar@slu.edu>
2017-09-23 21:01:49 -0500

    Update dr_clean1.R

--- R/dr_clean1.R
+++ R/dr_clean1.R
@@ -1,7 +1,7 @@
 dr_clean1 <- function(dataFrame,varName,calVal,calStd,correctVar) {
   #CleanVar1(dataframe, variable, cal. value, cal. std.)
   corrVal <- eval(substitute(correctVar), dataFrame)
-  raw <- eval(substitute(varName), dataframe)
+  raw <- eval(substitute(varName), dataFrame)
   correct <- raw + (corrVal*(calVal-calStd))
   return(correct)
 }
