89d34bd10877ca17cf3671b01a79c8840d93cb82
Andrew Shaughnessy <shaughnessyar@slu.edu>
2017-10-13 15:19:15 -0500

    working with testing

--- tests/testthat/full_test.R
+++ tests/testthat/full_test.R
@@ -3,7 +3,7 @@ context("series of commands to clean data")
 library(readr)
 suppressPackageStartupMessages(library(dplyr))
 
-test_data <- data("sondClean")
+test_data <- data("sondeClean")
 
 test_that("importing the data", {
   result <- dr_readSonde(system.file("extdata", "rawData.csv", package = "driftR"), defineVar = TRUE)
@@ -11,18 +11,18 @@ test_that("importing the data", {
 })
 
 test_that("creating correction factors", {
-  factors <- dr_correct(result, dateVar = Date, timeVar = Time, format = YMD)
+  factors <- dr_correct(result, dateVar = Date, timeVar = Time, format = "MDY")
   excect_equal(factors, test_data$corrFactors)
 })
 
 test_that("one point correction", {
-  factors <- dr_correct(result, dateVar = Date, timeVar = Time, format = YMD)
+  factors <- dr_correct(result, dateVar = Date, timeVar = Time, format = "MDY")
   result$SpCond_corr <- dr_clean1(result, SpCond, calVal = 1.07, calStd = 1, factors)
   expect_equal(result$SpCond_corr, test_data$SpCond_corr)
 })
 
 test_that("two point correction", {
-  factors <- dr_correct(result, dateVar = Date, timeVar = Time, format = YMD)
+  factors <- dr_correct(result, dateVar = Date, timeVar = Time, format = "MDY")
   result$pH_corr <- dr_clean2(result, pH, calValLow = 7.01, calStdLow = 7, calValHigh = 11.8, calStdHigh =  10, factors)
   expect_equal(result$pH_corr, test_data$pH_corr)
 })
