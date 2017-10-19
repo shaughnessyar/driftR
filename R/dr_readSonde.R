e8cdd7f9542df9fe7f3bb663e46303eeffcde38e
Christopher Prener, PhD <prenercg@slu.edu>
2017-10-19 16:25:16 -0500

    update assignment operator syntax, add @importFrom

--- R/dr_readSonde.R
+++ R/dr_readSonde.R
@@ -9,24 +9,26 @@
 #' dr_readSonde("~/fileLocation/data.csv", defineVar= TRUE)
 #'}
 #'
+#' @importFrom dplyr as_tibble()
+#'
 #' @export
 "dr_readSonde" <- function(fileExtention, defineVar=FALSE) {
   if (base::is.null(defineVar)) {
-    allContent = base::readLines(fileExtention)
-    df = utils::read.csv(base::textConnection(allContent), header = TRUE, stringsAsFactors = FALSE)
+    allContent <- base::readLines(fileExtention)
+    df <- utils::read.csv(base::textConnection(allContent), header = TRUE, stringsAsFactors = FALSE)
     df <- dplyr::as_tibble(df)
     return(df)
   }
   else if (defineVar == "TRUE"){
-    allContent = base::readLines(fileExtention)
-    skipSecond = allContent[-2]
-    df = utils::read.csv(base::textConnection(skipSecond), header = TRUE, stringsAsFactors = FALSE)
+    allContent <- base::readLines(fileExtention)
+    skipSecond <- allContent[-2]
+    df <- utils::read.csv(base::textConnection(skipSecond), header = TRUE, stringsAsFactors = FALSE)
     df <- dplyr::as_tibble(df)
     return(df)
   }
   else {
-    allContent = base::readLines(fileExtention)
-    df = utils::read.csv(base::textConnection(allContent), header = TRUE, stringsAsFactors = FALSE)
+    allContent <- base::readLines(fileExtention)
+    df <- utils::read.csv(base::textConnection(allContent), header = TRUE, stringsAsFactors = FALSE)
     df <- dplyr::as_tibble(df)
     return(df)
   }
