b93b20497a2720714ca4c17d784a62ebe14806fa
Christopher Prener <chris.prener@slu.edu>
2017-10-22 14:06:43 -0500

    fixes to documentation and functions based on R CMD check

--- R/dr_correctTwo.R
+++ R/dr_correctTwo.R
@@ -1,12 +1,13 @@
 #' Two-point drift correction
-#' @description This command takes the raw data from the water-quality instrument, utilizes the values generated from \code{\link{dr_correct}} and returns data that accounts for drift over time. This is done via a two point calibration standard, which it typical for pH and chloride.
-#' @param dataFrame The working data frame
-#' @param varName The name of the variable to correct
+#' @description This command takes the raw data from the water-quality instrument, utilizes the values generated from \code{\link{dr_factor}} and returns data that accounts for drift over time. This is done via a two point calibration standard, which it typical for pH and chloride.
+#' @param .data The working data frame
+#' @param sourceVar The name of the variable to correct
+#' @param cleanVar The new variable
 #' @param calValLow The number that the instrument was actually reading for the low standard
 #' @param calStdLow The number that the instrument should have been reading for that standard; i.e. the low standard value
 #' @param calValHigh The number that the instrument was actually reading for the high standard
 #' @param calStdHigh The number that the instrument should have been reading for that standard; i.e. the high standard value
-#' @param factorVar Name of value or variable generated from \code{\link{dr_correct}}
+#' @param factorVar Name of value or variable generated from \code{\link{dr_factor}}
 #' @return A list of values for the specified \code{varName} corrected for drift
 #' @examples
 #' \dontrun{
@@ -16,6 +17,9 @@
 #'
 #' @export
 dr_correctTwo <- function(.data, sourceVar, cleanVar, calValLow, calStdLow, calValHigh, calStdHigh, factorVar) {
+
+  # To prevent NOTE from R CMD check 'no visible binding for global variable'
+  high = low = NULL
 
   # quote input variables
   cleanVar <- quo_name(enquo(cleanVar))
