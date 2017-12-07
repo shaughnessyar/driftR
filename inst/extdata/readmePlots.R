1ddfff3addc5276fe3d3545d57f1d9a3293c4e80
Christopher Prener, PhD <prenercg@slu.edu>
2017-12-07 13:24:54 -0600

    update readme plots

--- inst/extdata/readmePlots.R
+++ inst/extdata/readmePlots.R
@@ -2,43 +2,45 @@ library(dplyr)
 library(ggplot2)
 
 df <- dr_readSonde(system.file("extdata", "rawData.csv", package = "driftR"), defineVar = TRUE)
-df <- dr_factor(df, corrFactor = corrFac, dateVar = Date, timeVar = Time, format = "MDY")
-df <- dr_correctTwo(df, sourceVar = pH, cleanVar = pH_Corr, calValLow = 7.01, calStdLow = 7,
-                    calValHigh = 11.8, calStdHigh =  10, factorVar = corrFac)
+df <- dr_factor(df, corrFactor = corrFac, dateVar = Date, timeVar = Time, format = "MDY", keepDateTime = TRUE)
+df <- dr_correctTwo(df, sourceVar = Chloride, cleanVar = Chloride_Corr,
+                    calValLow = 11.6, calStdLow = 10, calValHigh = 1411,
+                    calStdHigh =  1000, factorVar = corrFac)
 
 df %>%
-  dplyr::mutate(dateTime = stringr::str_c(Date, Time, sep = " ", collapse = NULL)) %>%
   dplyr::mutate(dateTime = base::as.POSIXct(dateTime, format = "%m/%d/%Y %H:%M:%S")) -> df
 
 ggplot(data = df) +
-  geom_smooth(mapping = aes(x = dateTime, y = pH, color = "pH")) +
-  geom_smooth(mapping = aes(x = dateTime, y = pH_Corr, color = "pH_Corr")) +
+  geom_smooth(mapping = aes(x = dateTime, y = Chloride, color = "Chloride")) +
+  geom_smooth(mapping = aes(x = dateTime, y = Chloride_Corr, color = "Chloride_Corr")) +
   labs(
-    title = "Comparison of Corrected and Uncorrected pH Values",
+    title = "Comparison of Corrected and Uncorrected Chloride Values",
     subtitle = "Equilibration Not Accounted For",
     x = "Date/Time",
-    y = "pH Value",
+    y = "Chloride Value",
     caption = "Created using the example data included in the driftR package;\nGeneralized additive model used for smoothing",
     colour = "Series"
   ) +
-  scale_colour_discrete(labels=c("Raw pH", "Corrected pH"))
+  scale_colour_discrete(labels=c("Uncorrected Chloride", "Corrected Chloride"))
 
 ggsave("man/figures/pH_noDrop.png", width = 200, height = 150, units = "mm", dpi = 100)
 
-df <- dr_drop(df, head=10, tail=6)
+df <- dr_drop(df, head=6, tail=6)
 
 ggplot(data = df) +
-  geom_smooth(mapping = aes(x = dateTime, y = pH, color = "pH")) +
-  geom_smooth(mapping = aes(x = dateTime, y = pH_Corr, color = "pH_Corr")) +
+  geom_smooth(mapping = aes(x = dateTime, y = Chloride, color = "Chloride")) +
+  geom_smooth(mapping = aes(x = dateTime, y = Chloride_Corr, color = "Chloride_Corr")) +
   labs(
-    title = "Comparison of Corrected and Uncorrected pH Values",
+    title = "Comparison of Corrected and Uncorrected Chloride Values",
     subtitle = "Equilibration Accounted For Using dr_drop()",
     x = "Date/Time",
-    y = "pH Value",
+    y = "Chloride Value",
     caption = "Created using the example data included in the driftR package;\nGeneralized additive model used for smoothing",
     colour = "Series"
   ) +
-  scale_colour_discrete(labels=c("Raw pH", "Corrected pH"))
+  scale_colour_discrete(labels=c("Uncorrected Chloride", "Corrected Chloride"))
 
 ggsave("man/figures/pH_Drop.png", width = 200, height = 150, units = "mm", dpi = 100)
+
+
 
