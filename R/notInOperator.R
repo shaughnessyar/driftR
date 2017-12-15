29f25df123e4d37890484dc6ce27daee4df47bc6
Christopher Prener <chris.prener@slu.edu>
2017-12-15 12:13:41 -0600

    fix non-canonical url in documentation

--- R/notInOperator.R
+++ R/notInOperator.R
@@ -3,7 +3,7 @@
 #' Provides the compliment to the base R \code{\%in\%} operator. Included here instead of via import
 #' due to stability issues with the source package, \href{https://github.com/harrelfe/Hmisc/blob/master/R/in.operator.s}{\code{Hmsic}},
 #' during original package development in October, 2017. Used under terms of
-#' \href{https://cran.r-project.org/web/packages/Hmisc/index.html}{\code{Hmisc}}'s
+#' \href{https://CRAN.R-project.org/package=Hmisc}{\code{Hmisc}}'s
 #' \href{https://cran.r-project.org/web/licenses/GPL-3}{GPL-3 License}.
 #'
 #' @param x vector or \code{NULL}: the values to be matched
