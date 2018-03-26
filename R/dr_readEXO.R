2c5f30f99e88d4a7dba0b8aaabe05bf1e5898eb0
Andrew Shaughnessy <shaughnessyar@slu.edu>
2018-03-26 10:48:29 -0500

    Adding documentation

--- R/dr_readEXO.R
+++ R/dr_readEXO.R
@@ -1,3 +1,26 @@
+#' Import raw data from a YSI EXO2 Sonde
+#'
+#' @description This function imports the raw data from a YSI EXO2 Sonde and formats the data set as a tibble.
+#'     If \code{defineVar} is set to \code{TRUE} (the default option), units of measurement will not be
+#'     included in the first observation.
+#'
+#' @usage dr_readEXO(file, defineVar = TRUE)
+#'
+#' @param file The name of the file which the data are to be read from. Each row of the table appears
+#'     as one line of the file. If it does not contain an absolute path, the file name is relative to
+#'     the current working directory.
+#' @param defineVar Logical scalar that determines if the units of measurement are included in the first
+#'     observation. If they are included, all vectors will be read in as character.
+#'
+#' @return A tibble with the formatted data and the variable types defined if \code{defineVar = TRUE}
+#'
+#' @examples
+#' \dontrun{
+#' dr_readEXO("data.xlsx")
+#' dr_readEXO("data.xlsx", defineVar = TRUE)
+#'}
+#'
+#' @export
 dr_readEXO <- function (file, defineVar = TRUE) {
   if (!file.exists(file)) {
     stop("File cannot be found. Check file name spelling and ensure it is saved in the working directory.")
--- R/dr_readHOBO.R
+++ R/dr_readHOBO.R
@@ -1,3 +1,27 @@
+#' Import raw data from an Onset Computer Corporation U24 Conductivity Logger
+#'
+#' @description This function imports the raw data from an Onset U24 Conductivity Logger and formats the data set as a tibble.
+#'     If \code{defineVar} is set to \code{TRUE} (the default option), units of measurement will not be
+#'     included in the first observation.
+#'
+#' @usage dr_readHOBO(file, defineVar = TRUE, fileFormat = c("TXT", "CSV"))
+#'
+#' @param file The name of the file which the data are to be read from. Each row of the table appears
+#'     as one line of the file. If it does not contain an absolute path, the file name is relative to
+#'     the current working directory.
+#' @param defineVar Logical scalar that determines if the units of measurement are included in the first
+#'     observation. If they are included, all vectors will be read in as character.
+#' @param fileFormat The format of the input file. HOBO data can be exported as both .csv and .txt files from the instrument.
+#'
+#' @return A tibble with the formatted data and the variable types defined if \code{defineVar = TRUE}
+#'
+#' @examples
+#' \dontrun{
+#' dr_readHOBO("data.txt", defineVar = TRUE, fileFormat = "TXT")
+#' dr_readHOBO("data.csv", defineVar = TRUE, fileFormat = "CSV")
+#'}
+#'
+#' @export
 dr_readHOBO <- function (file, defineVar = TRUE, fileFormat = c("TXT", "CSV")) {
   if (!file.exists(file)) {
     stop("File cannot be found. Check file name spelling and ensure it is saved in the working directory.")
