d906032857e4cb511be9b9842350cd2db0a27427
Andrew Shaughnessy <shaughnessyar@slu.edu>
2018-04-19 20:58:04 -0500

    unnest read sub-functions

--- R/dr_read.R
+++ R/dr_read.R
@@ -51,101 +51,101 @@ dr_read <- function(file, instrument, defineVar = TRUE){
     data <- readHOBO(file = file, defineVar = defineVar)
     return(data)
   }
+}
 
-  #defining subfunctions
-  #YSI Sonde 6600
-  readSonde <- function(file, defineVar = TRUE) {
+#defining subfunctions
+#YSI Sonde 6600
+readSonde <- function(file, defineVar = TRUE) {
 
-    if(!file.exists(file)){
-      stop('File cannot be found. Check file name spelling and ensure it is saved in the working directory.')
-    }
-
-    if (!(typeof(defineVar) %in% c('logical'))) {
-      stop(glue::glue('defineVar value {defineVar} not acceptable - value should TRUE or FALSE'))
-    }
-
-    if (defineVar == FALSE) {
-      allContent <- base::readLines(file)
-      df <- utils::read.csv(base::textConnection(allContent), header = TRUE, stringsAsFactors = FALSE)
-      df <- tibble::as_tibble(df)
-      return(df)
-    }
-    else if (defineVar == TRUE){
-      allContent <- base::readLines(file)
-      skipSecond <- allContent[-2]
-      df <- utils::read.csv(base::textConnection(skipSecond), header = TRUE, stringsAsFactors = FALSE)
-      df <- tibble::as_tibble(df)
-      return(df)
-    }
+  if(!file.exists(file)){
+    stop('File cannot be found. Check file name spelling and ensure it is saved in the working directory.')
   }
 
-  #YSI EXO2 Sonde
-  readEXO <- function (file, defineVar = TRUE) {
-    if (!file.exists(file)) {
-      stop("File cannot be found. Check file name spelling and ensure it is saved in the working directory.")
-    }
-    if (!(typeof(defineVar) %in% c("logical"))) {
-      stop(glue::glue("defineVar value {defineVar} not acceptable - value should TRUE or FALSE"))
-    }
-    if (defineVar == FALSE) {
-      df <- readxl::read_excel(file)
-      df <- tibble::as_tibble(df)
-      return(df)
-    }
-    else if (defineVar == TRUE) {
-      df <- readxl::read_excel(file)
-      a <- which(df[,1] == "1")[1]
-      df <- readxl::read_excel(file, skip = (a+1))
-      df$`Date (MM/DD/YYYY)` <- base::as.character(df$`Date (MM/DD/YYYY)`)
-      df$`Time (HH:MM:SS)` <- base::as.character(df$`Time (HH:MM:SS)`)
-      df$`Time (HH:MM:SS)` <-  base::gsub("^.*? ", "", df$`Time (HH:MM:SS)`)
-      df <- tibble::as_tibble(df)
-      return(df)
-    }
+  if (!(typeof(defineVar) %in% c('logical'))) {
+    stop(glue::glue('defineVar value {defineVar} not acceptable - value should TRUE or FALSE'))
   }
 
-  #Onset U24 Conductivity Logger
-  readHOBO <- function (file, defineVar = TRUE) {
+  if (defineVar == FALSE) {
+    allContent <- base::readLines(file)
+    df <- utils::read.csv(base::textConnection(allContent), header = TRUE, stringsAsFactors = FALSE)
+    df <- tibble::as_tibble(df)
+    return(df)
+  }
+  else if (defineVar == TRUE){
+    allContent <- base::readLines(file)
+    skipSecond <- allContent[-2]
+    df <- utils::read.csv(base::textConnection(skipSecond), header = TRUE, stringsAsFactors = FALSE)
+    df <- tibble::as_tibble(df)
+    return(df)
+  }
+}
 
-    # To prevent NOTE from R CMD check 'no visible binding for global variable'
-    . = NULL
+#YSI EXO2 Sonde
+readEXO <- function (file, defineVar = TRUE) {
+  if (!file.exists(file)) {
+    stop("File cannot be found. Check file name spelling and ensure it is saved in the working directory.")
+  }
+  if (!(typeof(defineVar) %in% c("logical"))) {
+    stop(glue::glue("defineVar value {defineVar} not acceptable - value should TRUE or FALSE"))
+  }
+  if (defineVar == FALSE) {
+    df <- readxl::read_excel(file)
+    df <- tibble::as_tibble(df)
+    return(df)
+  }
+  else if (defineVar == TRUE) {
+    df <- readxl::read_excel(file)
+    a <- which(df[,1] == "1")[1]
+    df <- readxl::read_excel(file, skip = (a+1))
+    df$`Date (MM/DD/YYYY)` <- base::as.character(df$`Date (MM/DD/YYYY)`)
+    df$`Time (HH:MM:SS)` <- base::as.character(df$`Time (HH:MM:SS)`)
+    df$`Time (HH:MM:SS)` <-  base::gsub("^.*? ", "", df$`Time (HH:MM:SS)`)
+    df <- tibble::as_tibble(df)
+    return(df)
+  }
+}
 
-    fileFormat <- substr(file, nchar(file)-2, nchar(file))
+#Onset U24 Conductivity Logger
+readHOBO <- function (file, defineVar = TRUE) {
 
-    if (!file.exists(file)) {
-      stop("File cannot be found. Check file name spelling and ensure it is saved in the working directory.")
+  # To prevent NOTE from R CMD check 'no visible binding for global variable'
+  . = NULL
+
+  fileFormat <- substr(file, nchar(file)-2, nchar(file))
+
+  if (!file.exists(file)) {
+    stop("File cannot be found. Check file name spelling and ensure it is saved in the working directory.")
+  }
+  if (!(typeof(defineVar) %in% c("logical"))) {
+    stop(glue::glue("defineVar value {defineVar} not acceptable - value should TRUE or FALSE"))
+  }
+  if (fileFormat %nin% c("TXT","txt","CSV","csv")) {
+    stop(glue::glue("The file format is invalid - format should be .txt or .csv"))
+  }
+  if (defineVar == FALSE) {
+    if (fileFormat == "TXT" | fileFormat == "txt"){
+      df <- readr::read_tsv(file, skip = 1)
     }
-    if (!(typeof(defineVar) %in% c("logical"))) {
-      stop(glue::glue("defineVar value {defineVar} not acceptable - value should TRUE or FALSE"))
+    if (fileFormat == "CSV" | fileFormat == "csv"){
+      df <- readr::read_csv(file, skip = 1)
     }
-    if (fileFormat %nin% c("TXT","txt","CSV","csv")) {
-      stop(glue::glue("The file format is invalid - format should be .txt or .csv"))
-    }
-    if (defineVar == FALSE) {
-      if (fileFormat == "TXT" | fileFormat == "txt"){
-        df <- readr::read_tsv(file, skip = 1)
-      }
-      if (fileFormat == "CSV" | fileFormat == "csv"){
-        df <- readr::read_csv(file, skip = 1)
-      }
-      df <- tibble::as_tibble(df)
-      return(df)
+    df <- tibble::as_tibble(df)
+    return(df)
 
-    } else if (defineVar == TRUE) {
-      if (fileFormat == "TXT" | fileFormat == "txt"){
-        df <- readr::read_tsv(file, skip = 1)
-      }
-      if (fileFormat == "CSV" | fileFormat == "csv"){
-        df <- readr::read_csv(file, skip = 1)
-      }
-      df <- df %>% stats::setNames(base::gsub(",.*?$","", base::names(.)))
-      df$date <- base::gsub(" .*$", "", df$`Date Time`)
-      df$time <- base::substr(df$`Date Time`, 10, base::nchar(df$`Date Time`))
-      df$time <- base::format(base::strptime(df$time, "%I:%M:%S %p"), "%H:%M:%S")
-      df$`Date Time` <- base::paste(df$date, df$time)
-      df <- tibble::as_tibble(df)
-
-      return(df)
+  } else if (defineVar == TRUE) {
+    if (fileFormat == "TXT" | fileFormat == "txt"){
+      df <- readr::read_tsv(file, skip = 1)
     }
+    if (fileFormat == "CSV" | fileFormat == "csv"){
+      df <- readr::read_csv(file, skip = 1)
+    }
+    df <- df %>% stats::setNames(base::gsub(",.*?$","", base::names(.)))
+    df$date <- base::gsub(" .*$", "", df$`Date Time`)
+    df$time <- base::substr(df$`Date Time`, 10, base::nchar(df$`Date Time`))
+    df$time <- base::format(base::strptime(df$time, "%I:%M:%S %p"), "%H:%M:%S")
+    df$`Date Time` <- base::paste(df$date, df$time)
+    df <- tibble::as_tibble(df)
+
+    return(df)
   }
 }
